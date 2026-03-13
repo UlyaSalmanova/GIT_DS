dcc4989fa5b3493799a35773e814966aa1d7c407
ulya_salmanova <danilova.u1994@ya.ru>
2026-03-13 14:13:19 +0300

    create pject_0.1

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ From the [Skillfactory Data Science course]( https://skillfactory.ru/data-scient
 ## Projects
 
 * [Project 0. Game: Guess the number](https://github.com/UlyaSalmanova/GIT_DS/tree/main/project_0)
+* [Project 0.1. Game: Guess the number. The final task](https://github.com/UlyaSalmanova/GIT_DS/tree/main/project_0.1_TheFinalTask)
---
+++ project_0.1_TheFinalTask/game_core_v3.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict = np.random.randint(1, 101)
+
+    while number != predict:
+        count += 1
+        if number > predict:
+            if number - predict > 10:
+                predict += 10
+            else:
+                predict += 1
+        elif number < predict:
+            if predict - number > 10:
+                predict -= 10
+            else:
+                predict -= 1
+
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+
+
+print('Run benchmarking for game_core_v3: ', end='')
+score_game(game_core_v3)
