9114b235a1e0cce659659911bd839ebbc6702be9
ulya_salmanova <danilova.u1994@ya.ru>
2026-04-07 12:18:22 +0300

    07.04.26

---
+++ practice.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+simplelist = [19, 242, 14, 152, 142, 1000]
+print(np.mean(simplelist))
+
+# np.random.seed(2021)
+# sample = np.random.rand()
+# randoms = np.random.uniform(-150, 2021, 120)
+# table = np.random.randint(1, 101, size=(3, 2))
+# even = np.arange(2, 17, 2)
+# mix = even.copy()
+# np.random.shuffle(mix)
+# select = np.random.choice(even, size=3, replace=False)
+# triplet = np.random.permutation(select)
+
+# print(sample)
+# print()
+# print(randoms)
+# print()
+# print(table)
+# print()
+# print(even)
+# print()
+# print(mix)
+# print()
+# print(select)
+# print()
+# print(triplet)
+# print()
+# print()
